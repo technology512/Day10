c915d6acbb8f9288cfcee6cb41838b74eb8f855d
technology512 <ald.wapper@gmail.com>
2021-03-18 01:37:54 +0100

    Lookups

---
+++ ex08/src/index.js
@@ -0,0 +1,21 @@
+function myFunction(email) {
+    var username = "";
+  
+    // Only change code below this line
+    var username = {
+      "user1@mail.com": "user1",
+      "user2@mail.com": "user2",
+      "user3@mail.com": "user3",
+      "user4@mail.com": "user4",
+      "user5@mail.com": "user5",
+      "user6@mail.com": "user6",
+    };
+  
+    var username = username[email];
+  
+    // Only change code above this line
+    return username;
+  }
+  
+  console.log(myFunction("user2@mail.com"));
+  module.exports = myFunction;
