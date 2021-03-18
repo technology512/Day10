42f000c6bb3414f5e57199f84fa4162106d94ddd
technology512 <ald.wapper@gmail.com>
2021-03-18 01:28:22 +0100

    Objects

---
+++ ex01/src/index.js
@@ -0,0 +1,16 @@
+// Only change code below this line
+
+var myPet = {
+    species: "Retriver",
+    name: "Rex",
+    legs: 4,
+    friends: ["Rocky", "Mickey"],
+  };
+  
+  function myFunction(myObj) {
+    return myObj;
+  }
+  
+  // Only change code above this line
+  console.log(myFunction(myPet)); // Change this line
+  module.exports = { myPet, myFunction };
